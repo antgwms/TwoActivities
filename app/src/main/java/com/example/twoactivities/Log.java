c85ecde1843f79767af473a7818110ba871245d0
Anthony Williams <antgwms@outlook.com>
2020-11-17 23:49:16 +0000

    Initial commit
    Created Two Activites project

---
+++ app/src/main/java/com/example/twoactivities/Log.java
@@ -0,0 +1,4 @@
+package com.example.twoactivities;
+
+public class Log {
+}
---
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button Clicked!");
+    }
+}
